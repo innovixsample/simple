6022206e39f5c638fec40904bb9faef27e9075e3
innovixsample <115861014+innovixsample@users.noreply.github.com>
2022-11-01 11:17:49 +0530

    073

--- sam/src/main/java/com/sam/SamApplication.java
+++ sam/src/main/java/com/sam/SamApplication.java
@@ -3,10 +3,10 @@ package com.sam;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-/*
-author sridhar 
-*/
-
+/**
+ * @author sridhar
+ *
+ */
 @SpringBootApplication
 public class SamApplication {
 
